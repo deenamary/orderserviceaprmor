ceadf816a367d716b492c572984463bc09fc7a6b
deena <deenaantonym@gmail.com>
2024-05-07 17:55:41 +0530

    Initial commit

---
+++ src/test/java/com/example/orderserviceaprmor/OrderserviceaprmorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderserviceaprmor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceaprmorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
